7e51a18f402ed5aa88280378797f088db2ec8ad2
William Chou <wwchou@ucla.edu>
2017-11-10 18:53:54 -0800

    add search backend

--- routes/spotify.js
+++ routes/spotify.js
@@ -23,7 +23,7 @@ var access_token;
 
 //AUTHORIZE ON /SPOTIFY REQ
 //main purpose: place access token in global var
-router.use('/', function(req, res){
+router.use('/auth', function(req, res){
   request.post(authOptions, function(error, response) {
     if (!error && response.statusCode == 200) {
       access_token = response.body.access_token;
@@ -32,6 +32,19 @@ router.use('/', function(req, res){
       console.log("error", response.statusCode);
     }
   });
+})
+
+router.use('/search', function(req, res) {
+  request.get({
+    url: 'https://api.spotify.com/v1/search?q=lil+uzi+vert&type=track&limit=5',
+    headers: {
+      'Authorization': 'Bearer ' + access_token,
+      'Content-Type': 'application/json'
+    }
+  }, function(error, response) {
+    console.log(response.body);
+    res.send(response.body);
+  })
 })
 
 module.exports = router;
