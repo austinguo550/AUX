4b3a8a82c442e9c73ae52ded5c5ad394caa5c520
Michael Yu <michael.yu2016@gmail.com>
2017-11-30 15:33:23 -0800

    update display and clean mainClient.js

--- public/javascripts/mainClient.js
+++ public/javascripts/mainClient.js
@@ -1,4 +1,8 @@
 var ownerID = ""
+var oath = {
+  access_token: "",
+  refresh_token: "",
+}
 
 async function createRoom() {
 	// TODO:
@@ -28,11 +32,6 @@ async function createRoom() {
 
 
 (function() {
-
-  /**
-   * Obtains parameters from the hash of the URL
-   * @return Object
-   */
   function getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
@@ -43,61 +42,18 @@ async function createRoom() {
     return hashParams;
   }
 
-  var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-      userProfileTemplate = Handlebars.compile(userProfileSource),
-      userProfilePlaceholder = document.getElementById('user-profile');
-
-  var oauthSource = document.getElementById('oauth-template').innerHTML,
-      oauthTemplate = Handlebars.compile(oauthSource),
-      oauthPlaceholder = document.getElementById('oauth');
-
   var params = getHashParams();
   var access_token = params.access_token,
       refresh_token = params.refresh_token,
       error = params.error;
 
-  if (error) {
-    alert('There was an error during the authentication');
-  } else {
-    if (access_token) {
-      // render oauth info
-      oauthPlaceholder.innerHTML = oauthTemplate({
-        access_token: access_token,
-        refresh_token: refresh_token
-      });
+  oath.access_token = access_token;
+  oath.refresh_token = refresh_token;
 
-      $.ajax({
-          url: 'https://api.spotify.com/v1/me',
-          headers: {
-            'Authorization': 'Bearer ' + access_token
-          },
-          success: function(response) {
-            userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+  console.log(oath);
 
-            $('#login').hide();
-            $('#loggedin').show();
-          }
-      });
-    } else {
-        // render initial screen
-        $('#login').show();
-        $('#loggedin').hide();
-    }
-
-    document.getElementById('obtain-new-token').addEventListener('click', function() {
-      $.ajax({
-        url: '/refresh_token',
-        data: {
-          'refresh_token': refresh_token
-        }
-      }).done(function(data) {
-        access_token = data.access_token;
-        oauthPlaceholder.innerHTML = oauthTemplate({
-          access_token: access_token,
-          refresh_token: refresh_token
-        });
-      });
-    }, false);
+  if(error) {
+    console.log("error with oath: ", error);
   }
 })();
 
