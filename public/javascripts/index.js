2a43f60f4b3980d84f03878f0fef5decf46ab618
William Chou <wwchou@ucla.edu>
2017-12-07 12:20:19 -0800

    deploy v2 try fix add song ux

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -82,6 +82,7 @@ async function chooseSong(song){
       const text = await response.text()
       if (status >= 200 && status < 300) {
       	console.log(text);
+      	mountpoint.innerHTML = "";
       	return;
       }else{
         console.log("error: ", text)
